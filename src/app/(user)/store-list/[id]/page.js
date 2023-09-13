4649e99d8e8566c71dbe74256f1cf7e04e072144
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:59:45 +0300

    ProductDetails ssr and contain ProductInformationPage

---
+++ src/app/(user)/store-list/[id]/page.js
@@ -0,0 +1,9 @@
+import ProductInformationPage from "@/components/pages/ProductInformationPage";
+
+export const metadata = {
+  title: "Product Information"
+}
+
+const ProductDetails = (props) => <ProductInformationPage {...props} />
+
+export default ProductDetails;
