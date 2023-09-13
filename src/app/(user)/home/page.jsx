ecf2eecd412efa31b7c53aa3f021e474de32d031
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:56:32 +0300

    home ssr contain HomeShowProductsPage

---
+++ src/app/(user)/home/page.jsx
@@ -0,0 +1,9 @@
+const { default: HomeShowProductsPage } = require("@/components/pages/HomeShowProductsPage");
+
+const metadata =  {
+  title: 'Home',
+}
+
+const home = () => <HomeShowProductsPage />
+
+export default home;
