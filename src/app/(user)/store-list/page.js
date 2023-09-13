14851a5e798073315462df805d135d81d908a880
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:57:46 +0300

    ProductsList ssr and contain ProductsPageList

---
+++ src/app/(user)/store-list/page.js
@@ -0,0 +1,9 @@
+import { ProductsPageList } from '@/components/pages/ProductsPageList'
+
+export const metadata = {
+  title: "Products List"
+}
+
+const ProductsList = () => < ProductsPageList />
+
+export default ProductsList;
