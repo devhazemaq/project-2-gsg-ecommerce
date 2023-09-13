6ac707c2d1a7b3feb4f9dfea36d7b4b343a0beba
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:46:42 +0300

    icon for login page

---
+++ src/app/(guest)/page.js
@@ -0,0 +1,16 @@
+import HomeShowProductsPage from "@/components/pages/HomeShowProductsPage";
+import Login from "./login/page";
+
+export const metadata = {
+  title: "Home",
+};
+
+const page = () => <HomeShowProductsPage/> 
+{/* <Login/> */}
+
+{/* <HomeShowProductsPage/> */}
+
+
+
+
+export default page;
