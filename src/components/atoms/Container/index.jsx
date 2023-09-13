06c697888b01e45692ea9459ec811bda7dd8fe56
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 16:07:12 +0300

    Container components

---
+++ src/components/atoms/Container/index.jsx
@@ -0,0 +1,12 @@
+"use client"
+import { StyledContainer } from './style'
+
+const Container = ({children}) => {
+  return (
+    <StyledContainer>
+      {children}
+    </StyledContainer>
+  )
+}
+
+export default Container
