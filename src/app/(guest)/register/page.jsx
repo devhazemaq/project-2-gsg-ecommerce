869556053952fb6f1b183316e4d0258ed95c5d5d
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:54:18 +0300

    register ssr  contain RegisterPage

---
+++ src/app/(guest)/register/page.jsx
@@ -0,0 +1,10 @@
+import RegisterPage from "@/components/pages/RegisterPage"
+
+
+export const metadata = {
+  title: 'Register',
+}
+
+const Register = () => <RegisterPage />
+
+export default Register; 
