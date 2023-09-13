c76da04e9ffa2c01666e495da867cc5b8202a51d
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-13 15:47:34 +0300

    files project main

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// const nextConfig = {}
+module.exports = {
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
+// module.exports = nextConfig
