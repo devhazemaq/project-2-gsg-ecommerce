6355a9162dc4060639704ea4370cf4a8e7ca9678
devhazemaq <hazemahmedqindeel@gmail.com>
2023-09-15 10:18:18 +0300

    LogBrand component he contain logo website

---
+++ src/components/atoms/LogoBrand/index.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+import { StyledLogoBr } from './style'
+
+const LogoBrand = () => {
+  return (
+    <StyledLogoBr>
+      <img src="/logoBrand1.svg" alt="logoBrand1"  />
+      <img src="/logoBrand2.svg" alt="logoBrand1"  />
+    </StyledLogoBr>
+  )
+}
+
+export default LogoBrand
---
+++ src/components/atoms/LogoBrand/style.js
@@ -0,0 +1,8 @@
+import { StyledFlex } from "@/styles/common";
+import { styled } from "styled-components";
+
+
+export const StyledLogoBr = styled(StyledFlex)`
+    width: 135px;
+    gap: 0.8rem;
+`;
